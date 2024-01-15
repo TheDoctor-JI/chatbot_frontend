eb6fa47dde0e1b61048d86830debefc6dc047db0
Dingdong-LIU <dliuak@connect.ust.hk>
2024-01-15 19:43:07 +0800

    add: frontend display table

---
+++ frontend_display_result.py
@@ -0,0 +1,49 @@
+# First
+import json
+import random
+import pandas as pd
+
+import requests
+import streamlit as st
+
+NGROK_DOMAIN = "http://eez115.ece.ust.hk:5000/"
+# NGROK_DOMAIN = "http://localhost:8080/"
+
+with st.sidebar:
+    session_id = st.text_input(
+        label="Please input your Session ID here:",
+        key="user_id",
+        type="default",
+        value="102292667",
+    )
+    domain = st.text_input(
+        "Please input the chatbot domain here:",
+        key="domain",
+        type="default",
+        value=NGROK_DOMAIN,
+    )
+    display_mode = st.selectbox(
+        label="Please select your display mode",
+        options=("normal", "log"),
+        key="select_display_mode",
+    )
+
+st.title("💬 PAF Result")
+
+
+
+if __name__ == "__main__":
+    # if "user_id" not in st.session_state or st.session_state.get("user_id") is None:
+    if not session_id:
+        st.info("Please input your Session ID on the left pane to start conversation.")
+        st.stop()
+    # if not domain:
+    #     st.info("Please input your domain on the left pane to start conversation.")
+    #     st.stop()
+    if domain != NGROK_DOMAIN:
+        NGROK_DOMAIN = domain
+
+    response = requests.post(f"{NGROK_DOMAIN}/get_paf_result", json={"session_id": session_id})
+    response = response.json()
+    table = pd.DataFrame.from_dict(response.get("responses", {}), orient='index')
+    st.table(table)
